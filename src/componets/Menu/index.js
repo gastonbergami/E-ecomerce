06e1a05a760267867754dd2f3e2ab62f8bd5f5cc
gastonbergami <gastonbergami@yahoo.com.ar>
2020-11-03 00:22:29 -0300

    first commit

---
+++ src/componets/Menu/index.js
@@ -0,0 +1,38 @@
+import react from 'react';
+import {Navbar,Nav,NavDropdown,Form,FormControl,Button} from 'react-bootstrap';
+import './index.css';
+
+const Menu = () =>{
+    return(
+        <>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">E-ecomerce</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Productos" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Bicicletas</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Patines</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Productos Niños</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Accesorios</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+        
+        
+        
+        </>
+    )
+}
+
+
+
+export default Menu;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import './index.css';
+import Menu from './componets/Menu';
+
+
+
+
+ReactDOM.render( 
+  <Menu/>,
+  document.getElementById('root')
+);
+
+
